d2c284e27181ca611c0b73ad2f15636b7220d9d1
Esteban Arias <estebanariaslima@gmail.com>
2022-01-10 19:40:25 -0400

    se empujan todos los cambios para que funcione el generador de cartas

---
+++ index.js
@@ -0,0 +1,38 @@
+let myNumberArray = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+
+let myPintaArray = ["♥","♣","♠","♦"];
+
+let divPinta = document.querySelectorAll(".pinta");
+let pPinta = document.createElement("p");
+
+divPinta.forEach( pPinta => {
+const p = document.createElement("p")
+pPinta.appendChild(p)
+})
+
+    function getRandomNumber() {
+    return myNumberArray [Math.floor(Math.random() * myNumberArray.length)];
+}
+
+document.querySelector("#numero").innerHTML = getRandomNumber();
+
+    function getRandomPinta() {
+    return myPintaArray [Math.floor(Math.random() * myPintaArray.length)];
+
+    }
+
+    window.onload= ()=>{;
+    pinta
+}
+    let pinta = getRandomPinta();
+
+    if (pinta === "♥" || pinta === "♦" ) {
+        document.querySelector("#pinta1").children[0].classList.add("red");
+        document.querySelector("#pinta2").children[0].classList.add("red");
+    } else {
+        document.querySelector("#pinta1").children[0].classList.add("black");
+        document.querySelector("#pinta2").children[0].classList.add("black");
+    }
+
+document.querySelector("#pinta1").children[0].innerHTML = pinta;
+document.querySelector("#pinta2").children[0].innerHTML = pinta;
